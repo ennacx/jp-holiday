95ba227e1f60ac4fe2f458c86d04a59477113a0e
Ennacx <enna.cx@gmail.com>
2025-03-05 11:53:32 +0900

    スケジュール実行し続けるための、GitHubActionsがアップデートする対象のファイルとロジック追加

--- main.php
+++ main.php
@@ -3,6 +3,9 @@ require 'vendor/autoload.php';
 
 try{
     (new JpHoliday\JpHoliday())->generate();
+
+    // スケジュール実行用のGit更新対象ファイルに実行日時を上書き
+    file_put_contents('.exec_timestamp', date('Y-m-d H:i:s'));
 } catch(\Exception $e){
     echo $e->getMessage();
 }
