2a88ea798efa4192019e44b4569291620ee2667d
Ennacx <enna.cx@gmail.com>
2025-07-24 17:37:58 +0900

    EnumのPHPDoc

--- src/Enums/CalFormat.php
+++ src/Enums/CalFormat.php
@@ -3,12 +3,22 @@ declare(strict_types=1);
 
 namespace JpHoliday\Enums;
 
+/**
+ * カレンダー出力形式
+ */
 enum CalFormat {
 
+    /** YYYY-MM-DD形式 */
     case DATE;
 
+    /** UNIXタイムスタンプ形式 */
     case TIMESTAMP;
 
+    /**
+     * 対応するファイル名
+     *
+     * @return string
+     */
     public function filename(): string {
         return match($this){
             self::DATE      => 'date',
@@ -16,6 +26,11 @@ enum CalFormat {
         };
     }
 
+    /**
+     * キー名変換
+     *
+     * @return string
+     */
     public function toString(): string {
         return match($this){
             self::DATE      => 'date',
--- src/Enums/CalType.php
+++ src/Enums/CalType.php
@@ -3,14 +3,21 @@ declare(strict_types=1);
 
 namespace JpHoliday\Enums;
 
+/**
+ * カレンダー生成タイプ
+ */
 enum CalType {
 
+    /** 祝日 */
     case SHU;
 
+    /** 祭日 */
     case SAI;
 
+    /** 祝祭日 */
     case BOTH;
 
+    /** 対応するディレクトリ名 */
     public function dirname(): ?string {
         return match($this){
             self::SHU => 'shu',
